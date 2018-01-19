d6cff7ed8d01dbfbee8a7340a34af0884719c11a
Solomah Yassir <solomahy@teach.cs.toronto.edu>
2018-01-18 19:16:29 -0500

    Extract

---
+++ a1_extractFeatures.py
@@ -0,0 +1,286 @@
+import numpy as np
+import sys
+import argparse
+import os
+import json
+import re
+import csv
+
+first_person = open("/u/cs401/Wordlists/First-person", "r")
+second_person = open("/u/cs401/Wordlists/Second-person", "r")
+third_person = open("/u/cs401/Wordlists/Third-person", "r")
+coord_conj = open("/u/cs401/Wordlists/Conjunct", "r")
+slang_one = open("/u/cs401/Wordlists/Slang", "r")
+slang_two = open("/u/cs401/Wordlists/Slang2", "r")
+
+
+first_person_words = []
+for line in first_person:
+    linesplit = line.split(" ")
+    for word in linesplit:
+        word = word.replace('\n', '')
+        first_person_words.append(word)
+
+second_person_words = []
+for line in second_person:
+    linesplit = line.split(" ")
+    for word in linesplit:
+        word = word.replace('\n', '')
+        second_person_words.append(word)
+
+third_person_words = []
+for line in third_person:
+    linesplit = line.split(" ")
+    for word in linesplit:
+        word = word.replace('\n', '')
+        third_person_words.append(word)
+
+conj_words = []
+for line in coord_conj:
+    linesplit = line.split(" ")
+    for word in linesplit:
+        word = word.replace('\n', '')
+        conj_words.append(word)
+
+slang_words = []
+for line in slang_one:
+    linesplit = line.split(" ")
+    for word in linesplit:
+        word = word.replace('\n', '')
+        slang_words.append(word)
+for line in slang_two:
+    linesplit = line.split(" ")
+    for word in linesplit:
+        word = word.replace('\n', '')
+        slang_words.append(word)
+
+future_tense_tags = ["'ll", "will"]
+common_noun_tags = ["NN", "NNS"]
+proper_noun_tags = ["NNP", "NNPS"]
+adverb_tags = ["RB", "RBR", "RBS"]
+
+AoA_words = {}
+IMG_words = {}
+FAM_words = {}
+with open('/u/cs401/Wordlists/BristolNorms+GilhoolyLogie.csv', 'r') as csvfile:
+    for line in csvfile.readlines():
+        line_content = line.split(",")
+        try:
+            AoA_words[line_content[1]] = int(line_content[3])
+            IMG_words[line_content[1]] = int(line_content[4])
+            FAM_words[line_content[1]] = int(line_content[5])
+        except ValueError:
+            continue
+
+V_words = {}
+A_words = {}
+D_words = {}
+with open('/u/cs401/Wordlists/Ratings_Warriner_et_al.csv', 'r') as csvfile:
+     for line in csvfile.readlines():
+        line_content = line.split(",")
+        try:
+            V_words[line_content[1]] = int(line_content[2])
+            A_words[line_content[1]] = int(line_content[5])
+            D_words[line_content[1]] = int(line_content[8])
+        except ValueError:
+            continue
+
+   
+
+
+def match_helper( comment, regex_list ):
+    count = 0
+    for regex in regex_list:
+        matches = re.findall(re.escape(regex + "/"), comment)
+        count += len(matches)
+    return (count)
+
+def extract1( comment ):
+    ''' This function extracts features from a single comment
+
+    Parameters:
+        comment : string, the body of a comment (after preprocessing)
+
+    Returns:
+        feats : numpy Array, a 173-length vector of floating point features
+    '''
+    print('TODO')
+    string_array = []
+    # TODO: your code here
+    feats = np.zeros(173)
+
+    # 1) FIRST PERSON
+    feats[0] = match_helper(comment, first_person_words)
+
+    # 2) SECOND PERSON
+    feats[1] = match_helper(comment, second_person_words)
+
+    # 3) THIRD PERSON
+    feats[2] = match_helper(comment, third_person_words)
+
+    # 4) CONJ
+    feats[3] = match_helper(comment, conj_words)
+
+    # 5) TAG PAST TENSE
+    feats[4] = len(re.findall("/vdb", comment, flags=re.IGNORECASE))
+
+    # 6) TAG FUTURE TENSE
+    feats[5] = match_helper(comment, future_tense_tags)
+    feats[5] += len(re.findall(r'[going/\S+ to/\S+ \S+/vb]{1}', comment, flags=re.IGNORECASE))
+
+    # 7) NUMBER COMMA
+    feats[6] = len(re.findall(" ,/", comment))
+
+    # 8) MULTI PUNCTUATION
+    feats[7] = len(re.findall(r'[ ' + re.escape(string.punctuation) + r'/' + re.escape(string.punctuation) + r']{2,}', comment))
+
+    # 9) COMMON NOUNS
+    for common_noun in common_noun_tags:
+        feats[8] += len(re.findall(r'[/' + common_noun + r' ]{1}', comment, flags=re.IGNORECASE))
+
+    # 10) PROPER NOUNS
+    for proper_noun in proper_noun_tags:
+        feats[9] += len(re.findall(r'[/' + proper_noun + r' ]{1}', comment, flags=re.IGNORECASE))
+
+    # 11) ADVERBS
+    for adverb in adverb_tags:
+        feats[10] += len(re.findall(r'[/' + adverb + r" ]{1}", comment, flags=re.IGNORECASE))
+
+    # 12) wh- WORDS
+    feats[11] = len(re.findall(r'[ wh\S+]{1}', comment, flags=re.IGNORECASE))
+
+    # 13) slang ACRO
+    for slang in slang_words:
+        feats[12] += len(re.findall(r' ' + re.escape(slang) + r'', comment, flags = re.IGNORECASE))
+
+    # 14) UPPERCASE
+    feats[13] = len(re.findall(r' [A-Z]{3,}[/]{1}', comment))
+
+    # 17) NUMBER SENTENCES
+    feats[16] = len(re.findall(r'' + re.escape("\n") + r'', comment))
+
+    # 15) LENGTH
+    feats[14] = len(re.findall(r'\S+/\S+', comment)) / feats[16]
+
+    # 16) LENGTH (NOT PUNCT)
+    toks = re.findall(r'( \w+/)', comment)
+    num_toks = len(toks)
+    total_length = 0
+    for tok in toks:
+        total_length += len(tok) - 1
+    feats[15] = total_length / num_toks
+
+    # 18, 19, 20) AVERAGE OF AoA, IMG, FAM
+    toksFound = 0
+    AoASum = 0
+    IMGSum = 0
+    FAMSum = 0
+    for tok in toks:
+        tok = tok.rstrip("/").lstrip(" ")
+        if(tok in AoA_words):
+            toksFound += 1
+            AoASum += AoA_words[tok]
+            IMGSum += IMG_words[tok]
+            FAMSum += FAM_words[tok]
+    if(toksFound > 0):
+        feats[17] = AoASum/toksFound
+        feats[18] = IMGSum/toksFound
+        feats[19] = FAMSum/toksFound
+
+
+    # 21, 22, 23) STD DEV OF AoA, IMG, FAM
+    toksFound = 0
+    AoASum = 0
+    IMGSum = 0
+    FAMSum = 0
+    for tok in toks:
+        tok = tok.rstrip("/").lstrip(" ")
+        if(tok in AoA_words):
+            toksFound += 1
+            AoASum += (AoA_words[tok] - feats[17])**2
+            IMGSum += (IMG_words[tok] - feats[18])**2
+            FAMSum += (FAM_words[tok] - feats[19])**2
+    if(toksFound > 0):
+        feats[20] = (AoASum/toksFound)**(1/2)
+        feats[21] = (IMGSum/toksFound)**(1/2)
+        feats[22] = (FAMSum/toksFound)**(1/2)
+
+
+    # 24, 25, 26) AVERAGE of V, A, D
+    toksFound = 0
+    VSum = 0
+    ASum = 0
+    DSum = 0
+    for tok in toks:
+        tok = tok.rstrip("/").lstrip(" ")
+        if(tok in AoA_words):
+            toksFound += 1
+            VSum += V_words[tok]
+            ASum += A_words[tok]
+            DSum += D_words[tok]
+    if(toksFound > 0):
+        feats[23] = VSum/toksFound
+        feats[24] = ASum/toksFound
+        feats[25] = DSum/toksFound
+
+
+    # 27, 28, 29) STD DEV OF V, A, D
+    toksFound = 0
+    VSum = 0
+    ASum = 0
+    DSum = 0
+    for tok in toks:
+        tok = tok.rstrip("/").lstrip(" ")
+        if(tok in AoA_words):
+            toksFound += 1
+            VSum += (V_words[tok] - feats[23])**2
+            ASum += (A_words[tok] - feats[24])**2
+            DSum += (D_words[tok] - feats[25])**2
+    if(toksFound > 0):
+        feats[26] = (VSum/toksFound)**(1/2)
+        feats[27] = (ASum/toksFound)**(1/2)
+        feats[28] = (DSum/toksFound)**(1/2)
+
+
+
+
+def main( args ):
+
+    data = json.load(open(args.input))
+    feats = np.zeros( (len(data), 173+1))
+
+    # TODO: your co
+    for i, line in enumerate(data):
+        j = json.loads(line)
+        comment = j['body']
+        type_comment = j['cat']
+
+        feats[i, 0:172] = extract1(comment)
+
+        if(type_comment == "Left"):
+            feats[i][173] = 0
+        elif(type_comment == "Center"):
+            feats[i][173] = 1
+        elif(type_comment == "Right"):
+            feats[i][173] = 2
+        elif(type_comment == "Alt"):
+            feats[i][173] = 3
+        else:
+            print("Unknown type: " + type_comment)
+            sys.exit(-1)
+
+
+
+    np.savez_compressed( args.output, feats)
+
+    
+if __name__ == "__main__": 
+
+    parser = argparse.ArgumentParser(description='Process each .')
+    parser.add_argument("-o", "--output", help="Directs the output to a filename of your choice", required=True)
+    parser.add_argument("-i", "--input", help="The input JSON file, preprocessed as in Task 1", required=True)
+    args = parser.parse_args()
+                 
+
+    main(args)
+
--- a1_preproc.py
+++ a1_preproc.py
@@ -10,6 +10,8 @@ import string
 
 indir = '/u/cs401/A1/data/';
 
+stopwords = open("/u/cs401/Wordlists/StopWords", "r")
+common_abbrev = open("/u/cs401/Wordlists/abbrev.english", "r")
 
 def preproc1( comment , steps=range(1,11) ):
     ''' This function pre-processes a single comment
@@ -24,51 +26,48 @@ def preproc1( comment , steps=range(1,11) ):
     
     comment_after_five = ""
     modComm = ''
-    print("Comment before: " + comment)
+#    print("Comment before: " + comment)
     if 1 in steps:
         comment = comment.replace('\n', '')
         comment = re.sub(r'[ ]+', " ", comment)
-        print("Comment after 1: " + comment)
+#        print("Comment after 1: " + comment)
     if 2 in steps:
         remove_html_escape = HTMLParser()
         comment = remove_html_escape.unescape(comment)
-        print("Comment after 2: " + comment)
+#        print("Comment after 2: " + comment)
     if 3 in steps:
         comment = re.sub(r'http\S*', '', comment)
         comment = re.sub(r'www\S*', '', comment)
-        print("Comment after 3: " + comment)
+#        print("Comment after 3: " + comment)
     if 4 in steps:
         punct = string.punctuation
         punct = punct.replace("'", "")
         comment = re.sub(r'([' + re.escape(punct) + r']+)', r' \1 ', comment)
-        print("Comment after 4.1: " + comment)
+#        print("Comment after 4.1: " + comment)
         comment = re.sub(r'([a-zA-Z] . [a-zA-Z . ]+)', r'\1'.replace(" ", ""), comment) 
-        print("Comment after 4.2: " + comment)
+#        print("Comment after 4.2: " + comment)
     if 5 in steps:
         comment = re.sub(r"([A-Za-z]{1}[']{1}[A-Za-z]{1})", r' \1', comment)
         comment = re.sub(r"([A-Za-z]{1}['] ])", r'\1'.replace("'", "") + " " + "'", comment)
         comment = re.sub(r'[ ]+', " ", comment)
         comment_after_five = comment
-        print("Comment after 5: " + comment)
+#        print("Comment after 5: " + comment)
     if 6 in steps:
-        print('TODO')
         new_comment_temp = ""
         nlp = spacy.load('en', disable=['parser', 'ner'])
         utt = nlp(u"" + comment + "")
         for token in utt:
             new_comment_temp = new_comment_temp + token.text + "/" + token.tag_ + " "
         comment = new_comment_temp
-        print("Comment after 6: " + comment)
+#        print("Comment after 6: " + comment)
     if 7 in steps:
         comment = " " + comment + " "
-        stopwords = open("/u/cs401/Wordlists/StopWords", "r")
         for line in stopwords:
             linesplit = line.split(" ")
             for stopword in linesplit:
                 stopword = stopword.replace('\n', '')
-                #print("stopword: " + "'" + stopword + "'")
                 comment = re.sub(r' ' + re.escape(stopword) + r'[/]\S+ ', ' ', comment, flags=re.IGNORECASE)
-        print("Comment after 7: " + comment)
+#        print("Comment after 7: " + comment)
     if 8 in steps:
         nlp = spacy.load('en', disable=['parser', 'ner'])
         utt = nlp(u"" + comment_after_five + "")
@@ -77,13 +76,41 @@ def preproc1( comment , steps=range(1,11) ):
                 continue
             else:
                 comment = re.sub(r'' + re.escape(token.text) + r'', token.lemma_, comment)
-        print("Comment after 8: " + comment)
-        sys.exit(0)
+#        print("Comment after 8: " + comment)
     if 9 in steps:
-        print('TODO')
+        split_comment = comment.split(" ")
+        new_comment = ""
+        for i in range(len(split_comment)):
+            if(len(split_comment[i]) == 0):
+                continue;
+            elif(i == 0):
+                new_comment = new_comment + split_comment[i] + " "
+                continue;
+            elif(split_comment[i][0] == '.'):
+                abbrev_flag = False
+                for line in common_abbrev:
+                    if(abbrev_flag == True):
+                        break;
+                    linesplit = line.split(" ")
+                    for abbrev in linesplit:
+                        abbrev = abbrev.replace('.', '')
+                        abbrev = abbrev.replace('\n', '')
+                        if(bool(re.search(" " + abbrev + "/", " " + split_comment[i-1]))):
+                            abbrev_flag = True
+                if(abbrev_flag == False):
+                    new_comment = new_comment + split_comment[i] + "\n"
+                else:
+                    new_comment = new_comment + split_comment[i] + " "
+            else:
+                new_comment = new_comment + split_comment[i] + " "
+        comment = new_comment
+#        print("Comment after 9: " + comment)
+
     if 10 in steps:
-        print('TODO')
-        
+        comment = comment.lower()
+#        print("Comment after 10: " + comment)
+
+    modComm = comment
     return modComm
 
 def main( args ):
@@ -93,7 +120,7 @@ def main( args ):
         for file in files:
             fullFile = os.path.join(subdir, file)
             print("Processing " + fullFile)
-
+            print("File name: " + file)
             data = json.load(open(fullFile))
 
             # TODO: select appropriate args.max lines
@@ -110,7 +137,7 @@ def main( args ):
                 print("i is: " + str(i))
                 line = data[i]
                 j = json.loads(line)
-                print("j is: " + str(j))
+ #               print("j is: " + str(j))
                 pre_processed_data = preproc1(j['body'])
                 j['body'] = pre_processed_data
                 j['cat'] = file
@@ -126,7 +153,7 @@ if __name__ == "__main__":
     parser.add_argument('ID', metavar='N', type=int, nargs=1,
                         help='your student ID')
     parser.add_argument("-o", "--output", help="Directs the output to a filename of your choice", required=True)
-    parser.add_argument("--max", help="The maximum number of comments to read from each file", default=10000)
+    parser.add_argument("--max", help="The maximum number of comments to read from each file", default=10000, type=int)
     args = parser.parse_args()
 
     if (args.max > 200272):
